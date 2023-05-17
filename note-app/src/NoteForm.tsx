4b046240862c9c4d9bd86d9ead32146b5c18ce1f
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-17 21:56:43 +0200

    note types

--- note-app/src/NoteForm.tsx
+++ note-app/src/NoteForm.tsx
@@ -1,24 +1,52 @@
-import { Form, Stack, Col, Row } from "react-bootstrap";
-import CreatablereactSelect from "react-select/creatable";
+import { Form, Stack, Col, Row, Button, FormEvent } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
+import { NoteData } from "./App";
 
-export function NoteForm() {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+export function NoteForm({ onSubmit }: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required />
+              <Form.Control ref={titleRef} required />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <Form.Select required />
+              <CreatableReactSelect isMulti />
             </Form.Group>
           </Col>
         </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
       </Stack>
     </Form>
   );
