1b5b1c2853ae17174456f0b852f23527d9932592
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-22 13:43:04 +0200

    Note

--- note-app/src/NoteLayout.tsx
+++ note-app/src/NoteLayout.tsx
@@ -1,5 +1,10 @@
 import { Note } from "./App";
-import { useParams, Navigate, Outlet } from "react-router-dom";
+import {
+  useParams,
+  Navigate,
+  Outlet,
+  useOutletContext,
+} from "react-router-dom";
 
 type NoteLayoutProps = {
   notes: Note[];
@@ -7,9 +12,13 @@ type NoteLayoutProps = {
 
 export function NoteLayout({ notes }: NoteLayoutProps) {
   const { id } = useParams();
-  const note = notes.find((n = n.id == id));
+  const note = notes.find((n) => n.id == id);
 
   if (note == null) return <Navigate to="/" replace />;
 
-  return <Outlet />;
+  return <Outlet context={note} />;
+}
+
+export function useNote() {
+  return useOutletContext<Note>();
 }
