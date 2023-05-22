5b373865434d2f4ea072a15a83b5379f58369564
xkostolanskym <kostolansky.marek@gmail.com>
2023-05-22 21:03:01 +0200

    delete note

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -72,6 +72,12 @@ function App() {
     });
   }
 
+  function onDelete(id: string) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id);
+    });
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -94,7 +100,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path="edit"
             element={
